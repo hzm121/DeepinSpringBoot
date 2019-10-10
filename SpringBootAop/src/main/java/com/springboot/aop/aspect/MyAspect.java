94b0c2dfba724b66e65977a41da3a7bdec57ded3
hzm121 <2696333036@qq.com>
2019-10-10 23:48:31 +0800

    complete arround

--- SpringBootAop/src/main/java/com/springboot/aop/aspect/MyAspect.java
+++ SpringBootAop/src/main/java/com/springboot/aop/aspect/MyAspect.java
@@ -1,8 +1,10 @@
 package com.springboot.aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Aspect
 public class MyAspect {
@@ -12,18 +14,24 @@ public class MyAspect {
     }
     @Before("pointCut()")
     public void before(){
-        System.out.println("before"+"-------"+ LocalDate.now().toString());
+        System.out.println("before"+"-------"+ LocalDateTime.now().toString());
+    }
+    @Around("pointCut()")
+    public void arround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("beforeArround");
+        joinPoint.proceed();
+        System.out.println("afterArround");
     }
     @After("pointCut()")
     public void after(){
-        System.out.println("after"+"-------"+ LocalDate.now().toString());
+        System.out.println("after"+"-------"+ LocalDateTime.now().toString());
     }
     @AfterReturning("pointCut()")
     public void afterReturning(){
-        System.out.println("afterReturning"+"-------"+ LocalDate.now().toString());
+        System.out.println("afterReturning"+"-------"+ LocalDateTime.now().toString());
     }
     @AfterThrowing("pointCut()")
     public void afterThrowing(){
-        System.out.println("afterThrowing"+"-------"+ LocalDate.now().toString());
+        System.out.println("afterThrowing"+"-------"+ LocalDateTime.now().toString());
     }
 }
